3b4cf7ce1c74d5be142088cc2ec35a394ecaaa6d
pickles-code <todesign0515@gmail.com>
2022-11-27 07:13:35 +0900

    HTML/CSS/JavaScript 初級編完了

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,13 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
+  console.log(e);
 
+  let checkbox = document.getElementById('check');
+  let text = document.getElementById('text');
+
+  if (checkbox.checked) {
+    text.style.backgroundColor = "#ff0000";
+  } else {
+    text.style.backgroundColor = "";
+  }
 }
---
+++ src/scripts/station15.js
@@ -0,0 +1,10 @@
+function displayList() {
+  let blocks = document.getElementById("fruits");
+  blocks.innerHTML = `
+      <ul>
+          <li>リンゴ</li>
+          <li>イチゴ</li>
+          <li>ブドウ</li>
+      </ul>
+  `;
+}
